d70b664a647eabdd1be6a9a366a96fb3ccd0bfe8
PetruhinAlexsey <a_petruhin@rambler.ru>
2024-10-11 15:19:54 +0300

    11.10.24

--- Calc/app/src/main/java/com/example/calc/MainActivity.java
+++ Calc/app/src/main/java/com/example/calc/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.calc;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,15 +12,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         EditText editTextAnswer=findViewById(R.id.editTextAnswer1);
+        TextView textViewNoAnswer=findViewById(R.id.textViewNoAnswer);
+        TextView textViewYesAnswer=findViewById(R.id.textViewYesAnswer);
+        Button buttonOtvet=findViewById(R.id.buttonOtvet);
+
+        buttonOtvet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editTextAnswer.getText().toString().equals("20")){
+                    textViewYesAnswer.setVisibility(View.VISIBLE);
+                    textViewNoAnswer.setVisibility(View.GONE);
+                }else {
+                    textViewNoAnswer.setVisibility(View.VISIBLE);
+                    textViewYesAnswer.setVisibility(View.GONE);
+                }
+            }
+        });
 
     }
 }
