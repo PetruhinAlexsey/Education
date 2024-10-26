b631b179db0b9c9468a067d6787a408fc067a6ba
PetruhinAlexsey <a_petruhin@rambler.ru>
2024-10-27 02:58:53 +0300

    27.10.24

--- GameScore/app/src/main/java/com/example/gamescore/MainActivity.java
+++ GameScore/app/src/main/java/com/example/gamescore/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.gamescore;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -10,8 +11,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private int score1=1;
-    private int score2=1;
+    private int score1=0;
+    private int score2=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +21,26 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textViewTeam1_1=findViewById(R.id.textViewTeam1_1);
         TextView textViewTeam2_2=findViewById(R.id.textViewTeam2_2);
-
+        //установка значения переменной score в TextView
         textViewTeam1_1.setText(String.valueOf(score1));
         textViewTeam2_2.setText(String.valueOf(score2));
+
+        textViewTeam1_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //увеличение счетчика по клику на TextView элемент
+                score1++;
+                textViewTeam1_1.setText(String.valueOf(score1));
+            }
+        });
+
+        textViewTeam2_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                score2++;
+                textViewTeam2_2.setText(String.valueOf(score2));
+            }
+        });
 
     }
 }
