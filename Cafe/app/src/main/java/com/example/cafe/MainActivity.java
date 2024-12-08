d904affb70e095b868709f1d51407f0899c03a7f
PetruhinAlexsey <a_petruhin@rambler.ru>
2024-12-08 13:40:25 +0300

    1

--- Cafe/app/src/main/java/com/example/cafe/MainActivity.java
+++ Cafe/app/src/main/java/com/example/cafe/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cafe;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Intent intent=new Intent(this, MainActivity2.class);
+        startActivity(intent);
     }
 }
