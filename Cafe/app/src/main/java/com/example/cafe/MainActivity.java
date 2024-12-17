3ff6051d1f911c239c9b4cdb0f2ddd4a802752a4
PetruhinAlexsey <a_petruhin@rambler.ru>
2024-12-18 00:14:38 +0300

    1

--- Cafe/app/src/main/java/com/example/cafe/MainActivity.java
+++ Cafe/app/src/main/java/com/example/cafe/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.cafe;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,11 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTextName;
+    private EditText editTextPassword;
+    private Button buttonInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent=new Intent(this, MainActivity3.class);
-        startActivity(intent);
+        initViews();
+//        Intent intent=new Intent(this, MainActivity3.class);
+//        startActivity(intent);
+    }
+    private void initViews(){
+        editTextName=findViewById(R.id.editTextName);
+        editTextPassword=findViewById(R.id.editTextPassword);
+        buttonInput=findViewById(R.id.buttonInput);
     }
 }
