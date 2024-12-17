ba648b1d386c92324e37eef4cbb2b1051326c7aa
PetruhinAlexsey <a_petruhin@rambler.ru>
2024-12-17 15:58:01 +0300

    1

--- Cafe/app/src/main/java/com/example/cafe/MainActivity.java
+++ Cafe/app/src/main/java/com/example/cafe/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent=new Intent(this,MainActivity2.class);
+        Intent intent=new Intent(this, MainActivity3.class);
         startActivity(intent);
     }
 }
