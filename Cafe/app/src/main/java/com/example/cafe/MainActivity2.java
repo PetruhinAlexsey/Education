5a9c29d4993c9e43388524a3b8adc18d8eb67d86
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-01-02 16:24:45 +0300

    Update MainActivity2.java

--- Cafe/app/src/main/java/com/example/cafe/MainActivity2.java
+++ Cafe/app/src/main/java/com/example/cafe/MainActivity2.java
@@ -10,16 +10,12 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    //константа
-    private static final String EXTRA_USER_NAME="userName";
+    //константа EXTRA_USER_NAME
+    private static final String EXTRA_USER_NAME = "userName";
 
     private TextView textViewPrivet;
 
@@ -37,38 +33,75 @@ public class MainActivity2 extends AppCompatActivity {
     private Spinner spinnerTest2;
     private Button buttonTest;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+    private String radioButton;
 
-        initVews();
-        String userName=getIntent().getStringExtra(EXTRA_USER_NAME);
-        String privet_mudak=getString(R.string.Privet_mudak);
-        //на место плейсхолдера-%s этот метод вставит userName
-        // (то что после запятой в параметрах метода)
-        String result=String.format(privet_mudak, userName);
-    }
-
-    public static Intent newIntent(Context context, String userName){
+    public static Intent newIntent(Context context, String userName) {
         //фабричный метод
         Intent intent = new Intent(context, MainActivity2.class);
         //передача имени пользователя по ключу
         intent.putExtra(EXTRA_USER_NAME, userName);
         return intent;
     }
-    private void initVews(){
-        textViewPrivet=findViewById(R.id.textViewPrivet);
-        radioGroupTest12=findViewById(R.id.radioGroupTest12);
-        rbTest1=findViewById(R.id.rbTest1);
-        rbTest2=findViewById(R.id.rbTest2);
-        textViewTest3=findViewById(R.id.textViewTest3);
-        checktext1=findViewById(R.id.checktext1);
-        checktext2=findViewById(R.id.checktext2);
-        checktext3=findViewById(R.id.checktext3);
-        spinnerTest1=findViewById(R.id.spinnerTest1);
-        spinnerTest2=findViewById(R.id.spinnerTest2);
-        buttonTest=findViewById(R.id.buttonTest);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        initVews();
+        putUserName();
+
+        radioGroupTest12.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int id) {
+                if (id==rbTest1.getId()){
+                    onRbTest1();
+                } else if (id==rbTest2.getId()) {
+                    onRbTest2();
+                }
+            }
+        });
+
+    }
+    private void onRbTest1(){
+        radioButton=getString(R.string.radioButtTest1);
+        String Test_test1Label=getString(R.string.Test_test1,radioButton);
+        textViewTest3.setText(Test_test1Label);
+    }
+    private void onRbTest2(){
+        radioButton=getString(R.string.radioButtTest2);
+        String Test_test1Label=getString(R.string.Test_test1,radioButton);
+        textViewTest3.setText(Test_test1Label);
+    }
+    //инициализация элементов активити
+    private void initVews() {
+        textViewPrivet = findViewById(R.id.textViewPrivet);
+        radioGroupTest12 = findViewById(R.id.radioGroupTest12);
+        rbTest1 = findViewById(R.id.rbTest1);
+        rbTest2 = findViewById(R.id.rbTest2);
+        textViewTest3 = findViewById(R.id.textViewTest3);
+        checktext1 = findViewById(R.id.checktext1);
+        checktext2 = findViewById(R.id.checktext2);
+        checktext3 = findViewById(R.id.checktext3);
+        spinnerTest1 = findViewById(R.id.spinnerTest1);
+        spinnerTest2 = findViewById(R.id.spinnerTest2);
+        buttonTest = findViewById(R.id.buttonTest);
+    }
+
+    private void putUserName() {
+        //новый метод подстановки строки на место плейсхолдера (%s)
+        String userName = getIntent().getStringExtra(EXTRA_USER_NAME);
+        String privet_mudak = getString(R.string.Privet_mudak, userName);
+        //на место плейсхолдера-%s этот метод вставит userName
+        // (то что после запятой в параметрах метода)
+        textViewPrivet.setText(privet_mudak);
+        //старый метод подстановки строки на место плейсхолдера (%s)
+        // String userName=getIntent().getStringExtra(EXTRA_USER_NAME);
+        // String privet_mudak=getString(R.string.Privet_mudak);
+        // //на место плейсхолдера-%s этот метод вставит userName
+        // // (то что после запятой в параметрах метода)
+        // String result=String.format(privet_mudak, userName);
+        // textViewPrivet.setText(result);
     }
 
 }
