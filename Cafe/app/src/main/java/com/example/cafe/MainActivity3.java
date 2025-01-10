dea8357f25af95b576bf25fdfb2e90da995951cd
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-01-10 18:15:04 +0300

    1

--- Cafe/app/src/main/java/com/example/cafe/MainActivity3.java
+++ Cafe/app/src/main/java/com/example/cafe/MainActivity3.java
@@ -13,25 +13,27 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity3 extends AppCompatActivity {
 
-    private static final String EXTRA_USER_NAME="userName";
-    private static final String EXTRA_RADIO_BUTTON="radioButton";
-    private static final String EXTRA_TEST_CHECKED="Test1OrTest2Checked";
-    private static final String EXTRA_CHECK_BOXES="checkboxes";
+    private static final String EXTRA_USER_NAME = "userName";
+    private static final String EXTRA_RADIO_BUTTON = "radioButton";
+    private static final String EXTRA_TEST_CHECKED = "Test1OrTest2Checked";
+    private static final String EXTRA_CHECK_BOXES = "checkboxes";
 
     private TextView textView1;
+    private TextView textView2;
+    private TextView textView3;
+    private TextView textView4;
 
 
     public static Intent newIntent(Context context,
                                    String userName,
                                    String radioButton,
                                    String Test1OrTest2Checked,
-                                   String checkboxes)
-    {
-        Intent intent=new Intent(context, MainActivity3.class);
-        intent.putExtra(EXTRA_USER_NAME,userName);
-        intent.putExtra(EXTRA_RADIO_BUTTON,radioButton);
-        intent.putExtra(EXTRA_TEST_CHECKED,Test1OrTest2Checked);
-        intent.putExtra(EXTRA_CHECK_BOXES,checkboxes);
+                                   String checkboxes) {
+        Intent intent = new Intent(context, MainActivity3.class);
+        intent.putExtra(EXTRA_USER_NAME, userName);
+        intent.putExtra(EXTRA_RADIO_BUTTON, radioButton);
+        intent.putExtra(EXTRA_TEST_CHECKED, Test1OrTest2Checked);
+        intent.putExtra(EXTRA_CHECK_BOXES, checkboxes);
         return intent;
     }
 
@@ -43,12 +45,18 @@ public class MainActivity3 extends AppCompatActivity {
         put();
     }
 
-    private void initViews(){
-            textView1=findViewById(R.id.textView1);
+    private void initViews() {
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        textView4 = findViewById(R.id.textView4);
     }
 
-    private void put(){
+    private void put() {
         textView1.setText(getIntent().getStringExtra(EXTRA_USER_NAME));
+        textView2.setText(getIntent().getStringExtra(EXTRA_RADIO_BUTTON));
+        textView3.setText(getIntent().getStringExtra(EXTRA_TEST_CHECKED));
+        textView4.setText(getIntent().getStringExtra(EXTRA_CHECK_BOXES));
     }
 
 }
