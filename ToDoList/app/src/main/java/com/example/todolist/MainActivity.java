adc3c56428e0636acc059ea678d9c52a163912a9
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-01-20 18:00:36 +0300

    1

--- ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.todolist;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +9,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayoutNotes;
+    private FloatingActionButton buttonAddNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
 
-
+    private void initViews(){
+        linearLayoutNotes=findViewById(R.id.linearLayoutNotes);
+        buttonAddNote=findViewById(R.id.buttonAddNote);
     }
 }
