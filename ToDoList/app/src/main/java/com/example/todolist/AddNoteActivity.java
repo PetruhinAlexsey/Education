94408a53d045d8ec877984c66919f1bab94562f3
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-02-18 18:58:34 +0300

    1

--- ToDoList/app/src/main/java/com/example/todolist/AddNoteActivity.java
+++ ToDoList/app/src/main/java/com/example/todolist/AddNoteActivity.java
@@ -1,6 +1,7 @@
 package com.example.todolist;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AddNoteActivity extends AppCompatActivity {
+
+    private EditText editTextNote;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
