83f7bf6b2ccde1cb72f456ceda0ea73d05f27338
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-02-27 19:39:08 +0300

    1

--- ToDoList/app/src/main/java/com/example/todolist/AddNoteActivity.java
+++ ToDoList/app/src/main/java/com/example/todolist/AddNoteActivity.java
@@ -1,6 +1,7 @@
 package com.example.todolist;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -21,13 +22,25 @@ public class AddNoteActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
         initViews();
+        //слушатель клика
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     private void initViews() {
-        editTextNote = findViewById(R.id.editTextNote);
-        radioButtonLow = findViewById(R.id.radioButtonLow);
-        radioButtonMedium = findViewById(R.id.radioButtonMedium);
-        radioButtonHigh = findViewById(R.id.radioButtonHigh);
-        buttonSave = findViewById(R.id.buttonSave);
+        editTextNote = findViewById(R.id.editTextNote);//ctrl+alt+shift - одноврменный курсор
+        radioButtonLow = findViewById(R.id.radioButtonLow);//ctrl+alt+shift - одноврменный курсор
+        radioButtonMedium = findViewById(R.id.radioButtonMedium);//ctrl+alt+shift - одноврменный курсор
+        radioButtonHigh = findViewById(R.id.radioButtonHigh);//ctrl+alt+shift - одноврменный курсор
+        buttonSave = findViewById(R.id.buttonSave);//ctrl+alt+shift - одноврменный курсор
     }
+    private void saveNote(){
+        String text = editTextNote.getText().toString().trim();
+    }
+
+
 }
