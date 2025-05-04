ad9828cfdf5808554791054673a36007b635c438
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-05-04 13:19:37 +0300

    1

--- ToDoListv1/app/src/main/java/com/example/todolistv1/MainActivity.java
+++ ToDoListv1/app/src/main/java/com/example/todolistv1/MainActivity.java
@@ -12,11 +12,20 @@ import androidx.core.view.WindowInsetsCompat;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
-private LinearLayout linearLayoutNotes;
-private FloatingActionButton buttonAddNote;
+    //поле с заметками
+    private LinearLayout linearLayoutNotes;
+    //кнопка добавления заметки
+    private FloatingActionButton buttonAddNote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+    }
+    //метод инициализации переменных
+    private void initViews(){
+        linearLayoutNotes=findViewById(R.id.linearLayoutNotes);
+        buttonAddNote=findViewById(R.id.buttonAddNote);
+
     }
 }
---
+++ ToDoListv1/app/src/main/java/com/example/todolistv1/Note.java
@@ -0,0 +1,11 @@
+package com.example.todolistv1;
+
+public class Note {
+    //поля класса
+    private int id;
+    //будем отображать это поле
+    private String text;
+    //поле приоритет
+    private int priority;
+
+}
