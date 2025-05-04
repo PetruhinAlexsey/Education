50a67ede8fcde3741e40261c9cf30ae2f5968c63
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-05-04 16:22:03 +0300

    1

--- ToDoListv1/app/src/main/java/com/example/todolistv1/Note.java
+++ ToDoListv1/app/src/main/java/com/example/todolistv1/Note.java
@@ -5,7 +5,25 @@ public class Note {
     private int id;
     //будем отображать это поле
     private String text;
-    //поле приоритет
+    //поле приоритет,
+    // чем выше число тем выше приоритет у этой заметки
     private int priority;
+    //Конструктор
+    public Note(int id, String text, int priority) {
+        this.id = id;
+        this.text = text;
+        this.priority = priority;
+    }
+    //Геттеры на все поля
+    public int getId() {
+        return id;
+    }
 
+    public String getText() {
+        return text;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
 }
