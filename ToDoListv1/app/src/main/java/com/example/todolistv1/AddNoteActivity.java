98c0efd1ec0dbe136e430d82d2e2b33a7702cf35
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-05-24 21:51:06 +0300

    Update AddNoteActivity.java

--- ToDoListv1/app/src/main/java/com/example/todolistv1/AddNoteActivity.java
+++ ToDoListv1/app/src/main/java/com/example/todolistv1/AddNoteActivity.java
@@ -1,6 +1,9 @@
 package com.example.todolistv1;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +14,12 @@ import androidx.core.view.WindowInsetsCompat;
 public class AddNoteActivity extends AppCompatActivity {
 
     //ссылки на элементы activity_add_note.xml
+    private EditText editTextNote;
+    private RadioButton radioBattonLow;
+    private RadioButton radioBattonMedium;
+    private RadioButton radioBattonHigh;
+    private Button buttonSave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,4 +27,13 @@ public class AddNoteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_add_note);
 
     }
+    //присвоим значение в методе initViews
+    private void initViews(){
+        editTextNote=findViewById(R.id.editTextNote);
+        radioBattonLow=findViewById(R.id.radioBattonLow);
+        radioBattonMedium=findViewById(R.id.radioBattonMedium);
+        radioBattonHigh=findViewById(R.id.radioBattonHigh);
+        buttonSave=findViewById(R.id.buttonSave);
+    }
+
 }
