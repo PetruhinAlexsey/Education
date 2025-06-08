c0db7efb0e0e8d348cb95e118e058565a7c30146
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-06-08 20:35:34 +0300

    Update Database.java

--- ToDoListv1/app/src/main/java/com/example/todolistv1/Database.java
+++ ToDoListv1/app/src/main/java/com/example/todolistv1/Database.java
@@ -7,5 +7,25 @@ import java.util.ArrayList;
 
 public class Database {
     //Коллеция новых заметок
-    private ArrayList<Note> notes = new ArrayList<>();
+    private ArrayList<Note> notes=new ArrayList<>();
+
+    //в коллекцию notes добавляем новый объект note
+    private void add(Note note){
+
+        notes.add(note);
+    }
+    //удаляем заметку по ее id
+    public void remove(int id){
+        //в этом цикле мы получаем элемент из коллекции notes
+        for (int i=0;i< notes.size();i++){
+            //сохраняем эелемент в переменную note типа Note
+            Note note=notes.get(i);
+            if (note.getId()==id){
+                //удаляем элемент note из коллекции notes
+                notes.remove(note);
+            }
+        }
+    }
+
+
 }
