60eb01f3a698ccddc88bba23e13dd89561904424
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-06-20 12:07:32 +0300

    1

--- ToDoListv1/app/src/main/java/com/example/todolistv1/MainActivity.java
+++ ToDoListv1/app/src/main/java/com/example/todolistv1/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         notesAdapter=new NotesAdapter();
         //сказать private RecyclerView recyclerViewNotes какой адаптер применять
         recyclerViewNotes.setAdapter(notesAdapter);
-        //необходимо сообщить recyclerViewNotes каким образом отображать элементы
+
         
 
 
@@ -75,61 +75,6 @@ public class MainActivity extends AppCompatActivity {
     }
     //метод для деманстрации заметок на экране
     private void showNotes(){
-        //перед добавлением заметок linearLayoutNotes надо очищать
-        linearLayoutNotes.removeAllViews();
-        //тип цикла foreach, перебирает note в коллекции notes
-        for(Note note:database.getNotes()){
-            //в linearLayoutNotes надо
-            // добавить данный макет - note_item.xml
-            //но его надо преобразовать во view элемент
-            // с помощью getLayoutInflater()
-            //R.layout.note_item - что помещаем (макет note_item.xml)
-            //linearLayoutNotes - куда помещаем
-            //всегда false третьим параметром (что то про прикрепление)
-            //сохраняем в переменную типа View
-                //преобразование к типу View
-           View view=getLayoutInflater().inflate(
-                   R.layout.note_item,
-                   linearLayoutNotes,
-                   false
-           );
-           //удаление заметки при помощи клика на заметку
-           view.setOnClickListener(new View.OnClickListener() {
-               @Override
-               public void onClick(View v) {
-                   database.remove(note.getId());
-                   showNotes();
-               }
-           });
-            TextView textViewNote=view.findViewById(R.id.textViewNote);
-
-                textViewNote.setText(note.getText());
-
-                //присваиваем цвет заметке switch case
-            //так мы получаем только id цвета
-            //сам цвет еще нужно получить
-            int colorResId;
-                switch (note.getPriority()){
-                    //если приоритет 0 то ставим зеленый
-                    case 0:colorResId= android.R.color.holo_green_light;
-                    break;
-                    //если приоритет 1 то ставим желтый
-                    case 1:colorResId= android.R.color.holo_orange_light;
-                        break;
-                    //по умолчанию значение colorResId присваивается красный
-                    default:colorResId= android.R.color.holo_red_light;
-                }
-            //просто посмотреть
-            //textViewNote.setText(Integer.toString(colorResId)); - тест, какой id цвета
-
-            //получаем сам цвет
-            //в ContextCompat.getColor передаем контекст - this
-            //и colorResId - id цвета
-            int color= ContextCompat.getColor(this,colorResId);
-            //устанавливаем цвет заметки
-            textViewNote.setBackgroundColor(color);
-           //помещаем view в linearLayoutNotes
-           linearLayoutNotes.addView(view);
-        }
+        notesAdapter.setNotes(database.getNotes());
     }
 }
