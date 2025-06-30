cd050db5373eaff060a4e1433a7eedb280f52aea
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-06-30 23:44:57 +0300

    1

---
+++ stepik.org_course_198363/IntelliJ_IDEA/Test1/src/Main.java
@@ -0,0 +1,23 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(10+5);
+        int a=66;
+        int b=600;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        String name="huilo";
+        System.out.println("Привет "+name);
+        double a1=0.2;
+        double b1=0.3;
+        System.out.println(a1+b1);
+        System.out.println(a1-b1);
+        System.out.println(a1/b1);
+        System.out.println(a1*b1);
+        System.out.println(5==6);
+    }
+}
