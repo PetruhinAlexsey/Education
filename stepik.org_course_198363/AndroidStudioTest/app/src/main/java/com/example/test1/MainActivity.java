1d86f1929191eb3b6b6b77deda70c277664bcc41
PetruhinAlexsey <a_petruhin@rambler.ru>
2025-07-10 22:39:08 +0300

    1

--- stepik.org_course_198363/AndroidStudioTest/app/src/main/java/com/example/test1/MainActivity.java
+++ stepik.org_course_198363/AndroidStudioTest/app/src/main/java/com/example/test1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.test1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +13,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    int counter=0;
+    Button button;
+    TextView textView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    counter++;
+                    textView.setText(Integer.toString(counter));
+            }
+        });
 
     }
+
+    void initViews(){
+        button=findViewById(R.id.button);
+        textView=findViewById(R.id.textView);
+    }
+
+
 }
